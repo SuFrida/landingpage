74ddab9c2bdd70334a65336432fb46c129e1823b
SuFrida <frida_b_v@hotmail.com>
2022-03-27 15:19:27 -0600

    Base Vue Code

---
+++ src/components/Home.vue
@@ -0,0 +1,303 @@
+<template>
+  <div class="home container-fluid m-0 p-0 full-height">
+    <div class="navbar mt-0 d-flex">
+      <img src="../assets/NAGASE.png" alt="red-logo" />
+    </div>
+    <!-- Landing -->
+    <div class="row">
+      <div class="landing full-height col-12 p-2">
+        <div class="col-lg-6 col-sm-12 d-inline-block">
+          <h1>Acceleration Through Collaboration</h1>
+        </div>
+        <div class="col-lg-6 col-sm-12 d-inline-block">
+          <h2>
+            EMPOWER YOUR PEOPLE AND PROJECTS WITH NAGASE BUSINESS SERVICES
+          </h2>
+          <p>
+            Leveraging years of experience in international Specialty Chemicals
+            sales and marketing combined with a global network of over 100
+            Nagase Group® companies, NAGASE Business Services brings light to
+            your project lifecycle through accelerated access to a spectrum of
+            services: Materials Development, Production, Marketing, Regulatory,
+            and Deployment. Our goal is to be more than just your investor,
+            incubator, toll producer or marketer. We want to become your
+            collaborator.
+          </p>
+        </div>
+        <div type="button" class="btn btn-primary">Click To Learn</div>
+      </div>
+    </div>
+
+    <!-- Cards -->
+    <div class="cards full-height container-fluid">
+      <div class="cards-title">
+        <h1>
+          NAGASE BUSINESS SERVICES OFFERS A SPECTRUM OF A-LA-CARTE OR 360
+          SERVICES TO BRING YOUR PROJECTS TO THEIR FULL POTENTIAL
+        </h1>
+      </div>
+      <div
+        id="carouselExampleControls"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-12 col-md-4">
+                  <div class="card" style="width: 18rem" id="materials">
+                    <div class="card-body">
+                      <h5 class="card-title">Materials Development</h5>
+                      <p class="card-text">
+                        Nagase’s team of material science experts can develop a
+                        wide array of technologies that will allow you to
+                        rapidly iterate your ideas into viable materials. Our
+                        state-of-the-art advanced materials facility is
+                        centrally located in the US for maximum efficiency in
+                        delivering turnkey solutions.
+                      </p>
+                      <ul class="cards-options">
+                        <li>IDEA-TO-PRODUCT</li>
+                        <li>PRODUCT-MARKET FIT</li>
+                        <li>DE-RISKING</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-12 col-md-4">
+                  <div class="card" id="production" style="width: 18rem">
+                    <div class="card-body">
+                      <h5 class="card-title">Production</h5>
+                      <p class="card-text">
+                        As part of its global production network, Nagase has
+                        facilities in the US that provide toll manufacturing
+                        support. NBS leverages those capabilities to offer you
+                        customized support that best fits your production needs,
+                        be it at lab, pilot, or commercial scale.
+                      </p>
+                      <ul class="cards-options">
+                        <li>SCALE UP</li>
+                        <li>CUSTOM MANUFACTURING</li>
+                        <li>PROCESS OPTIMIZATION</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-12 col-md-4">
+                  <div class="card" id="marketing" style="width: 18rem">
+                    <div class="card-body">
+                      <h5 class="card-title">Marketing</h5>
+                      <p class="card-text">
+                        NAGASE Business Services is your trusted partner to
+                        investigate new end-usages and verticals, obtain
+                        customer voice feedback, and generate viable application
+                        data. In addition, NBS uses a variety of proven methods
+                        and tools to tailor campaigns that maximize commercial
+                        opportunities domestically or internationally.
+                      </p>
+                      <ul class="cards-options">
+                        <li>GO-TO MARKET IMPLEMENTATION</li>
+                        <li>MARKET IMPLEMENTATION</li>
+                        <li>LEAD GENERATION</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-12 col-md-4">
+                  <div class="card" id="regulatory" style="width: 18rem">
+                    <div class="card-body">
+                      <h5 class="card-title">Regulatory</h5>
+                      <p class="card-text">
+                        Through its extensive international distribution
+                        business, Nagase has developed an intimate regulatory
+                        knowledge that provides you with the assurance that all
+                        compliance aspects will be fully managed. Also, NBS has
+                        detailed experience in registration requirements when
+                        introducing new chemical products to verticals around
+                        the world.
+                      </p>
+                      <ul class="cards-options">
+                        <li>PRODUCT REGISTRATION</li>
+                        <li>IMPORT/EXPORT COMPLIANCE</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-12 col-md-4">
+                  <div class="card" id="deploy" style="width: 18rem">
+                    <div class="card-body">
+                      <h5 class="card-title">Deployment</h5>
+                      <p class="card-text">
+                        Our sales forces are technical experts who understand
+                        your products and invest themselves in their promotion
+                        and growth. When your project reaches commercial
+                        success, NBS is your partner to guarantee your materials
+                        are delivered anywhere in the world on time every time
+                        using our extensive experience in domestic and
+                        international logistics.
+                      </p>
+                      <ul class="cards-options">
+                        <li>DISTRIBUTION</li>
+                        <li>GLOBAL SALES REACH</li>
+                        <li>WAREHOUSING</li>
+                        <li>LOGISTICS</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <!-- Contact Form -->
+    <div class="contact-form full-height col-lg-6">
+      <h1>Make Your Business Shine</h1>
+      <h1>WITH NBS IN YOUR CORNER, THE POSSIBILITIES ARE ENDLESS</h1>
+      <p>
+        Connect with us and explore what NAGASE Business Services can do for
+        your project. Whether you have a material that’s ready to scale and need
+        a better understanding of its market and applications or you want to
+        commercialize it but are unsure about regulatory requirements, NBS can
+        empower your organization with flexible solutions tailored to your
+        specific needs.
+      </p>
+      <p>
+        Please fill out the contact form and one of our experts will reach out
+        to you soon.
+      </p>
+    </div>
+    <!-- Footer -->
+    <div class="footer full-height col-lg-12"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.home {
+  background: url(../assets/5.png) no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-position: right top;
+  background-attachment: fixed;
+}
+
+.navbar img {
+  /*Checar medida exacta*/
+  max-height: 15vh;
+  background-color: #af103f;
+  padding: 30px 20px 20px 20px;
+}
+
+.navbar {
+  padding-left: 50px;
+  padding-top: 0px !important;
+}
+
+.card {
+  border-radius: 0px !important;
+}
+
+.carousel-inner {
+  align-content: center !important;
+}
+
+#materials {
+  background-color: #a9d3c8;
+}
+
+#production {
+  background-color: #f2dda1;
+}
+
+#marketing {
+  background-color: #ebb3a3;
+}
+
+#regulatory {
+  background-color: #efc1ca;
+}
+
+#deploy {
+  background-color: #a1a9d6;
+}
+
+@media only screen and (max-width: 768px) {
+  [class*="col-"] {
+    width: 100%;
+  }
+
+  .navbar {
+    align-items: center;
+  }
+}
+</style>
